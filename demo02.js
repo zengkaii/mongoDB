ce03ba7f300766b40f0da71228bc4503e8af8a1a
zengkai <1965599425@qq.com>
2018-07-22 23:33:12 +0800

    js查询如何在文本中使用

--- demo02.js
+++ demo02.js
@@ -1,3 +1,134 @@
+// var workmate1={
+//     name:'JSPang',
+//     age:33,
+//     sex:1,
+//     job:'前端',
+//     skill:{
+//         skillOne:'HTML+CSS',
+//         skillTwo:'JavaScript',
+//         skillThree:'PHP'
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var workmate2={
+//     name:'ShengLei',
+//     age:31,
+//     sex:1,
+//     job:'JAVA后端',
+//     skill:{
+//         skillOne:'HTML+CSS',
+//         skillTwo:'J2EE',
+//         skillThree:'PPT'
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var workmate3={
+//     name:'MinJie',
+//     age:18,
+//     sex:0,
+//     job:'UI',
+//     skill:{
+//         skillOne:'PhotoShop',
+//         skillTwo:'UI',
+//         skillThree:'PPT'
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var workmate4={
+//     name:'XiaoWang',
+//     age:25,
+//     sex:1,
+//     job:'UI',
+//     skill:{
+//         skillOne:'PhotoShop',
+//         skillTwo:'UI',
+//         skillThree:'PPT'
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var workmate5={
+//     name:'LiangPeng',
+//     age:28,
+//     sex:1,
+//     job:'前端',
+//     skill:{
+//         skillOne:'HTML+CSS',
+//         skillTwo:'JavaScript',
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var workmate6={
+//     name:'HouFei',
+//     age:25,
+//     sex:0,
+//     job:'前端',
+//     skill:{
+//         skillOne:'HTML+CSS',
+//         skillTwo:'JavaScript',
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var workmate7={
+//     name:'LiuYan',
+//     age:35,
+//     sex:0,
+//     job:'美工',
+//     skill:{
+//         skillOne:'PhotoShop',
+//         skillTwo:'CAD',
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var workmate8={
+//     name:'DingLu',
+//     age:20,
+//     sex:0,
+//     job:'美工',
+//     skill:{
+//         skillOne:'PhotoShop',
+//         skillTwo:'CAD',
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var workmate9={
+//     name:'JiaPeng',
+//     age:29,
+//     sex:1,
+//     job:'前端',
+//     skill:{
+//         skillOne:'HTML+CSS',
+//         skillTwo:'JavaScript',
+//         skillThree:'PHP'
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var workmate10={
+//     name:'LiJia',
+//     age:26,
+//     sex:0,
+//     job:'前端',
+//     skill:{
+//         skillOne:'HTML+CSS',
+//         skillTwo:'JavaScript',
+//         skillThree:'PHP'
+//     },
+//     regeditTime:new Date(),
+//     interest:[]
+// }
+// var db=connect('company');
+// var workmateArray=[workmate1,workmate2,workmate3,workmate4,workmate5,workmate6,workmate7,workmate8,workmate9,workmate10];
+// db.workmate.insert(workmateArray);
+// print('[SUCCESS]：The data was inserted successfully');
+
 var workmate1={
     name:'JSPang',
     age:33,
@@ -9,8 +140,9 @@ var workmate1={
         skillThree:'PHP'
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['看电影','看书','吃美食','钓鱼','旅游']
 }
+
 var workmate2={
     name:'ShengLei',
     age:31,
@@ -22,8 +154,9 @@ var workmate2={
         skillThree:'PPT'
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['篮球','看电影','做饭']
 }
+
 var workmate3={
     name:'MinJie',
     age:18,
@@ -35,7 +168,7 @@ var workmate3={
         skillThree:'PPT'
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['做饭','画画','看电影']
 }
 var workmate4={
     name:'XiaoWang',
@@ -48,7 +181,7 @@ var workmate4={
         skillThree:'PPT'
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['写代码','篮球','画画']
 }
 var workmate5={
     name:'LiangPeng',
@@ -60,8 +193,9 @@ var workmate5={
         skillTwo:'JavaScript',
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['玩游戏','写代码','做饭']
 }
+
 var workmate6={
     name:'HouFei',
     age:25,
@@ -72,8 +206,9 @@ var workmate6={
         skillTwo:'JavaScript',
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['化妆','读书','做饭']
 }
+
 var workmate7={
     name:'LiuYan',
     age:35,
@@ -84,8 +219,10 @@ var workmate7={
         skillTwo:'CAD',
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['画画','聚会','看电影']
 }
+
+
 var workmate8={
     name:'DingLu',
     age:20,
@@ -96,8 +233,9 @@ var workmate8={
         skillTwo:'CAD',
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['美食','看电影','做饭']
 }
+
 var workmate9={
     name:'JiaPeng',
     age:29,
@@ -109,8 +247,9 @@ var workmate9={
         skillThree:'PHP'
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['写代码','篮球','游泳']
 }
+
 var workmate10={
     name:'LiJia',
     age:26,
@@ -122,8 +261,11 @@ var workmate10={
         skillThree:'PHP'
     },
     regeditTime:new Date(),
-    interest:[]
+    interest:['玩游戏','美食','篮球']
 }
+
+
+
 var db=connect('company');
 var workmateArray=[workmate1,workmate2,workmate3,workmate4,workmate5,workmate6,workmate7,workmate8,workmate9,workmate10];
 db.workmate.insert(workmateArray);
